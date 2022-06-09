543d9fccaa7343c9c73a03fb3d6d96e672896cb8
Pabllot07 <pabllot07@outlook.com>
2022-06-09 14:53:20 -0300

    Slider created

---
+++ src/data.js
@@ -0,0 +1,25 @@
+const sliderItems = [
+    {
+        id: 1,
+        img: "images/girl.jpg",
+        title: "WINTER SALE",
+        desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS",
+        bg: "transparent"
+    },
+    {
+        id: 2,
+        img: "images/gatinha.jpg",
+        title: "SUMMER SALE",
+        desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS",
+        bg: "transparent"
+    },
+    {
+        id: 3,
+        img: "images/clothes.jpg",
+        title: "POPULAR",
+        desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS",
+        bg: "transparent"
+    },
+]
+
+export default sliderItems
